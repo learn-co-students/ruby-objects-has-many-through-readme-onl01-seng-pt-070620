298b27d45bc4706ffa3eb919e037a6d0b4ac3f49
github username <jhernandez1692@gmail.com>
2020-08-04 19:30:59 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,20 @@
 class Meal
+    
+    attr_accessor :customer, :waiter, :tip, :total
+
+    @all = []
+
+    def initialize (waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip =  tip
+        @@all << self
+    end
+
+    def self.all
+     @@all
+    end
+
 
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,42 @@
+require "pry"
+
 class Waiter
+
+    attr_accessor :name, :yoe
+
+    @@all = []
+
+    def initialize(name,yoe)
+        @name = name
+        @yoe = yoe
+        @@all << self
+       
+    end
+
+    def self.all
+     @@all 
+    end
+
+    def meals
+        Meal.all.select {|meal|
+        meal.waiter == self}
+        
+    end
+     
+    def waiters
+        meals.map  {|meal| meal.waiter}
+    end
+     
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+     
+    def best_tipper
+        best_tipped_meal = meals.max do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip
+        end
+       
+        best_tipped_meal.customer
+      end
 
 end
