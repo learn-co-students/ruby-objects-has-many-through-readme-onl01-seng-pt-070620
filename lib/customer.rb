b40c01177f4b8664e4f819bd719d704e350364dc
Jbeltrez <beltrezjohn@gmail.com>
2020-08-12 18:15:00 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,24 @@
 class Customer
-  
+  # We want to make sure when building out classes, that there's something to store each instance
+  attr_accessor :name, :age 
+  @@all = []
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all << self 
+  end 
+  def self.all 
+    @@all 
+  end 
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end 
+    def meals 
+        Meal.all.select do |meal| meal.customer == self 
+        end 
+    end 
+    def waiters 
+        meals.map do |meal| meal.waiter 
+        end 
+    end 
 end
